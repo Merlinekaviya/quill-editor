2d2644f3e1629ea5a1cc73d8549b9fd85dd6515e
Merlinekaviya <kaviyamerline97@gmail.com>
2026-01-22 06:50:41 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,10 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld("gitAPI", {
+  getGitStatus: (workspacePath) =>
+    ipcRenderer.invoke("get-git-status", workspacePath)
+});
+
 contextBridge.exposeInMainWorld(
   'electronAPI',
   {
